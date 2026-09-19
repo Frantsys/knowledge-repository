b032650c5d8efc1573b83f4206f421dd5d8ae409
gilberto00000 <gilbertomendesgm189@gmail.com>
2026-09-19 13:20:45 -0300

    refactor: clean up SecurityConfig by reorganizing CorsConfigurationSource method

--- api/src/main/java/com/frantsys/knowledge_repository/config/SecurityConfig.java
+++ api/src/main/java/com/frantsys/knowledge_repository/config/SecurityConfig.java
@@ -31,32 +31,33 @@ public class SecurityConfig {
 
         return http.build();
     }
-    
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(
+            List.of("http://localhost:3000")
+        );
+
+        configuration.setAllowedMethods(
+            List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        );
+
+        configuration.setAllowedHeaders(
+            List.of("*")
+        );
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
     
 }
 
-@Bean
-public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-
-    configuration.setAllowedOrigins(
-        List.of("http://localhost:3000")
-    );
-
-    configuration.setAllowedMethods(
-        List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-    );
-
-    configuration.setAllowedHeaders(
-        List.of("*")
-    );
-
-    configuration.setAllowCredentials(true);
-
-    UrlBasedCorsConfigurationSource source =
-        new UrlBasedCorsConfigurationSource();
-
-    source.registerCorsConfiguration("/**", configuration);
-
-    return source;
-}
+    
