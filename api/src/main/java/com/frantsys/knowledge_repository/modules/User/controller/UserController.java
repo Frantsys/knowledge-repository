1d1eab13e1f9279111ba3249f7d23370ee589acd
gilberto00000 <gilbertomendesgm189@gmail.com>
2026-09-19 12:53:04 -0300

    refactor: reorganize imports and improve method formatting in UserController

--- api/src/main/java/com/frantsys/knowledge_repository/modules/User/controller/UserController.java
+++ api/src/main/java/com/frantsys/knowledge_repository/modules/User/controller/UserController.java
@@ -1,20 +1,5 @@
 package com.frantsys.knowledge_repository.modules.User.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.frantsys.knowledge_repository.modules.User.dto.UserUpdateActivationRequest;
-import com.frantsys.knowledge_repository.modules.User.dto.UserCreateRequest;
-import com.frantsys.knowledge_repository.modules.User.dto.UserUpdatePasswordRequest;
-import com.frantsys.knowledge_repository.modules.User.dto.UserResponse;
-import com.frantsys.knowledge_repository.modules.User.dto.UserSummaryResponse;
-import com.frantsys.knowledge_repository.modules.User.service.UserService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -23,61 +8,82 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.frantsys.knowledge_repository.modules.User.dto.UserCreateRequest;
+import com.frantsys.knowledge_repository.modules.User.dto.UserResponse;
+import com.frantsys.knowledge_repository.modules.User.dto.UserSummaryResponse;
+import com.frantsys.knowledge_repository.modules.User.dto.UserUpdateActivationRequest;
+import com.frantsys.knowledge_repository.modules.User.dto.UserUpdatePasswordRequest;
+import com.frantsys.knowledge_repository.modules.User.service.UserService;
 
-@Tag(name = "User", description = "API path for managing users")
-@RestController 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@Tag(
+    name = "User",
+    description = "API path for managing users"
+)
+@RestController
 @RequestMapping("/v1/api/users")
 @RequiredArgsConstructor
 public class UserController {
-    
+
     private final UserService userService;
 
     @GetMapping
     public ResponseEntity<List<UserSummaryResponse>> findAll() {
 
         List<UserSummaryResponse> users = userService.findAll();
-        return ResponseEntity.ok(users);
 
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserResponse> findById(@PathVariable Long id) {
-        
+    public ResponseEntity<UserResponse> findById(
+            @PathVariable Long id
+    ) {
+
         UserResponse user = userService.findById(id);
 
         return ResponseEntity.ok(user);
-    
     }
 
     @PostMapping
-    public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserCreateRequest request) {
-        
+    public ResponseEntity<UserResponse> createUser(
+            @RequestBody @Valid UserCreateRequest request
+    ) {
+
         UserResponse user = userService.createUser(request);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
-
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(user);
     }
 
     @PatchMapping("/{id}/activation")
-    public ResponseEntity<UserResponse> updateStatus(@PathVariable Long id, @RequestBody @Valid UserUpdateActivationRequest request) {
-        
-        UserResponse updatedUser = userService.updateActivationById(id, request);
+    public ResponseEntity<UserResponse> updateStatus(
+            @PathVariable Long id,
+            @RequestBody @Valid UserUpdateActivationRequest request
+    ) {
+
+        UserResponse updatedUser =
+                userService.updateActivationById(id, request);
 
         return ResponseEntity.ok(updatedUser);
-
     }
 
     @PatchMapping("/{id}/password")
-    public ResponseEntity<UserResponse> changePasswordById(@PathVariable Long id, @RequestBody @Valid UserUpdatePasswordRequest request) {
-        
+    public ResponseEntity<UserResponse> changePasswordById(
+            @PathVariable Long id,
+            @RequestBody @Valid UserUpdatePasswordRequest request
+    ) {
+
         userService.updatePassword(id, request);
 
         return ResponseEntity.noContent().build();
-        
     }
-
-    
-    
-
 }
